c238669144498a843186befffbde7b631b846e2e
pongsakorn-maker <pongsakorn.reo@gmail.com>
2021-05-22 10:52:46 +0700

    renew project structure

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "Leo";
+  print(name);
+  Map<String, dynamic> address = {
+    "homeNo": "90",
+    "postCode": "10900",
+  };
+  print(address);
+  print(address["homeNo"]);
+}
